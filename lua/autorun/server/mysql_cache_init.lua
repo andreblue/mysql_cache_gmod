22b5e14ef3d310a67b23026e472a2d2f5780e01c
Andreblue <andreblue2u@live.com>
2017-08-19 16:13:02 -0400

    Basic Functions up and running. Should be ready to go.

--- lua/autorun/server/mysql_cache_init.lua
+++ lua/autorun/server/mysql_cache_init.lua
@@ -1,5 +1,126 @@
-MsgC(Color(8,65,92), "[Mysql Cache] Starting up.\n")
+MsgC(Color(255,0,0), "[Mysql Cache] Starting up.\n")
 MysqlCache = {}
-
+MysqlCache.Timer = "MySQLCacheSyncTimer"
+MysqlCache.DB = {}
 --Version Checker
-MysqlCache.Version = Vector(1, 0, 0)
+MysqlCache.Version = {1, 0, 0}
+timer.Simple(30,function()
+http.Fetch("https://raw.githubusercontent.com/andreblue/mysql_cache_gmod/master/version",
+function(body, size, headers, code)
+  local remotever = string.Split( body, " " )
+  if MysqlCache.Version ~= remotever then
+    need_update = false
+    if MysqlCache.Version[1] < tonumber(remotever[1]) then
+      need_update = true
+    elseif MysqlCache.Version[2] < tonumber(remotever[2]) then
+      need_update = true
+    elseif MysqlCache.Version[3] < tonumber(remotever[3]) then
+      need_update = true
+    end
+    if need_update then
+      MsgC(Color(255,0,0), "[Mysql Cache] Please update. There is a new version.\n")
+    end
+  end
+end,
+function(errorMsg)
+  MsgC(Color(255,0,0), "[Mysql Cache] HTTP Error.\n")
+  MsgN(errorMsg)
+end)
+end)
+
+
+
+--Basic Functions
+function MysqlCache.RegisterDB(database_id, db_obj)
+  if MysqlCache.DB[database_id] then
+    return false, "Database ID already exists. If you need to update the object, then simply use the UpdateDB function"
+  end
+  MysqlCache.DB[database_id] = {}
+  MysqlCache.DB[database_id].obj = db_obj
+  return true, "Added " .. database_id
+end
+function MysqlCache.UpdateDB(database_id, db_obj)
+  if MysqlCache.DB[database_id] then
+    MysqlCache.DB[database_id].obj = db_obj
+    return true, "Update " .. database_id .. " object"
+  else
+    return false, "Database ID does not exists. You need to register it first with the RegisterDB function"
+  end
+end
+function MysqlCache.RegisterTable(database_id, table_name)
+  if MysqlCache.DB[database_id] then
+    if not MysqlCache.DB[database_id].Tables then
+      MysqlCache.DB[database_id].Tables = {}
+    end
+    if MysqlCache.DB[database_id].Tables[table_name] then
+      return false, "Table " .. table_name .. " already exists"
+    else
+      MysqlCache.DB[database_id].Tables[table_name] = true
+      MysqlCache.DB[database_id].refresh_time = 60
+      MysqlCache.DB[database_id].next_time = CurTime() + 60
+      return true, "Table " .. table_name .. " added"
+    end
+  else
+    return false, "Database ID does not exists. You need to register it first with the RegisterDB function"
+  end
+end
+function MysqlCache.UnregisterTable(database_id, table_name)
+  if MysqlCache.DB[database_id] then
+    if not MysqlCache.DB[database_id].Tables then
+      MysqlCache.DB[database_id].Tables = {}
+    end
+    if MysqlCache.DB[database_id].Tables[table_name] then
+      return false, "Table " .. table_name .. " does not exists"
+    else
+      MysqlCache.DB[database_id].Tables[table_name] = false
+      return true, "Table " .. table_name .. " removed"
+    end
+  else
+    return false, "Database ID does not exists. You need to register it first with the RegisterDB function"
+  end
+end
+function MysqlCache.SetRefreshTime(database_id, refresh_time)
+  if MysqlCache.DB[database_id] then
+    MysqlCache.DB[database_id].refresh_time = refresh_time
+    MysqlCache.DB[database_id].next_time = CurTime() + refresh_time
+    return true, "Updated time for " .. database_id
+  end
+  return false, "Database ID does not exists. You need to register it first with the RegisterDB function"
+end
+function MysqlCache.GetTable(database_id, table_name)
+  if MysqlCache.DB[database_id] then
+    if MysqlCache.DB[database_id].Tables and MysqlCache.DB[database_id].Tables[table_name] then
+      if MysqlCache.DB[database_id].Tables[table_name] == true then
+        return false, "Table data has not been pulled yet. Try again in a minute or so."
+      end
+      return MysqlCache.DB[database_id].Tables[table_name]
+    else
+      return false, "Table does not exists. You need to register it first with the RegisterTable function"
+    end
+  end
+  return false, "Database ID does not exists. You need to register it first with the RegisterDB function"
+end
+
+function MysqlCache.RunTimer()
+  for database_id, _ in pairs(MysqlCache.DB) do
+    if MysqlCache.DB[database_id].next_time and MysqlCache.DB[database_id].next_time <= CurTime() then
+      MysqlCache.DB[database_id].next_time = CurTime() + MysqlCache.DB[database_id].refresh_time
+      --Update Tables
+      for table_name, _ in pairs(MysqlCache.DB[database_id].Tables) do
+        local query = MysqlCache.DB[database_id].obj:query("SELECT * FROM " .. table_name)
+        function query:onSuccess(data)
+          MysqlCache.DB[database_id].Tables[table_name] = data
+        end
+        function query:onError( err, sql )
+          MsgC(Color(255,0,0), "[Mysql Cache] Error running query.\n")
+          MsgC(Color(255,0,0), "[Mysql Cache] " .. err .. " \n")
+          MsgC(Color(255,0,0), "[Mysql Cache] " .. sql .. " \n")
+        end
+        query:start()
+
+      end
+    end
+  end
+end
+
+timer.Create( MysqlCache.Timer, 1, 0, MysqlCache.RunTimer )
---
+++ lua/test_mysql_cache.lua
@@ -0,0 +1,56 @@
+--Test File
+-- lua_openscript test_mysql_cache.lua
+require( "mysqloo" )
+
+db = mysqloo.connect( "127.0.0.1", "root", "password", "usync", 3306 )
+
+function db:onConnected()
+
+    print( "Database has connected!" )
+
+    local q = self:query( "SELECT 5+5" )
+    function q:onSuccess( data )
+
+    end
+
+    function q:onError( err, sql )
+
+        print( "Query errored!" )
+        print( "Query:", sql )
+        print( "Error:", err )
+
+    end
+
+    q:start()
+
+end
+
+function db:onConnectionFailed( err )
+
+    print( "Connection to database failed!" )
+    print( "Error:", err )
+
+end
+
+db:connect()
+
+--Test Lines
+print(MysqlCache.RegisterDB('usync', db))
+print(MysqlCache.RegisterTable('usync', 'usync_users'))
+print(MysqlCache.SetRefreshTime('usync', 20))
+timer.Simple(1, function()
+  local result, message = MysqlCache.GetTable('usync', 'usync_users')
+  if result == false then
+    print(message)
+  else
+    PrintTable(result)
+  end
+end)
+timer.Simple(30, function()
+  local result, message = MysqlCache.GetTable('usync', 'usync_users')
+  if result == false then
+    print(message)
+  else
+    PrintTable(result)
+  end
+end)
